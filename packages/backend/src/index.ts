84cc0136e0a38f0aee71dece8629d9c08ef76ede
tjololo <1145298+tjololo@users.noreply.github.com>
2024-11-16 17:00:45 +0100

    make github integration work

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -17,6 +17,7 @@ backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
@@ -50,5 +51,8 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+
+//scaffolder
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 
 backend.start();
